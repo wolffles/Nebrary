ae43c233563b30026dbe6332cbd79136c05e8f86
woffles <wolfgang.truong@gmail.com>
2019-02-18 15:27:54 -0800

    working route

---
+++ routes/api/tv.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const router = express.Router();
+
+const https = require('https');
+var options = {
+  "method": "GET",
+  "hostname": "api.themoviedb.org",
+  "port": null,
+  "path": "/3/discover/tv?include_null_first_air_dates=false&timezone=America%2FNew_York&page=1&sort_by=popularity.desc&language=en-US&api_key=fb6a1d3f38c3d97f67df6d141f936f29",
+  "headers": {}
+};
+// @route GET api/tv/test
+// @desc Tests tv route
+// @access Public
+router.get('/test', (req,res) => res.json({message: "tv works"}));
+
+// @route GET api/tv/
+// @desc landing page
+// @access Public
+router.get('/', (req, res) => {
+  var request = https.request(options, function (resp) {
+    var body = '';
+    resp.on('data', function (chunk) {
+      body += chunk;
+    });
+    resp.on('end', function () {
+      let obj = JSON.parse(body);
+      res.json(obj)
+    });
+  }).on('error', function (e) {
+    res.json({message: "Got an error: "+  e});
+  });
+  // ends the http 
+  request.end()
+})
+module.exports = router;
--- server.js
+++ server.js
@@ -1,40 +1,14 @@
 const express = require('express');
 const tv = require('./routes/api/tv')
+
+// server app
 const app = express()
 
 app.use(express.json());
 app.use(express.urlencoded({extended:false}));
 
-const https = require('https');
-var options = {
-  "method": "GET",
-  "hostname": "api.themoviedb.org",
-  "port": null,
-  "path": "/3/discover/tv?include_null_first_air_dates=false&timezone=America%2FNew_York&page=1&sort_by=popularity.desc&language=en-US&api_key=fb6a1d3f38c3d97f67df6d141f936f29",
-  "headers": {}
-};
-
-app.get('/', (req, res) => {
-  
-  var request = https.get(options, function (resp) {
-    var body = '';
-
-    resp.on('data', function (chunk) {
-      body += chunk;
-    });
-
-    resp.on('end', function () {
-      let obj = JSON.parse(body);
-      console.log("Got a response: ", obj);
-      res.json(obj)
-    });
-  }).on('error', function (e) {
-    console.log("Got an error: ", e);
-  });
-  request.end()
-})
-
-// app.use('', tv);
+// use Routes
+app.use('/api/tv', tv);
 
 const port = process.env.PORT || 5000;
 
