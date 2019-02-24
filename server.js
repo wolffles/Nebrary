2d50711f5d4ebf240500008e5679f5780adce06b
woffles <wolfgang.truong@gmail.com>
2019-02-24 12:11:25 -0800

    prepared for deploy

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const express = require('express');
+const path = require('path')
+// server app
 const tv = require('./routes/api/tv')
 
-// server app
 const app = express()
 
 app.use(express.json());
@@ -9,6 +10,17 @@ app.use(express.urlencoded({extended:false}));
 
 // use Routes
 app.use('/api/tv', tv);
+
+// if none of these api routes are being hit, look for index
+// Server static assets if in production
+if(process.env.NODE_ENV === 'production') {
+  // Set static folder to client/build
+  app.use(express.static('client/build'));
+    // any route gets hit here load the react html file in build
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 const port = process.env.PORT || 5000;
 
